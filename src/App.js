081b40eab823ce60683cd0432a74254ad5263bfa
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-01-30 00:10:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import SpecificProductPage from './pages/SpecficProductPage';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <Router>
         <Switch>
-          <Route path="/" exact component={Navbar} />
+          <Route path="/" exact component={Home} />
+          <Route path="/productpage/:id" component={SpecificProductPage} />
         </Switch>
     </Router>
   );
---
+++ src/pages/SpecficProductPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function SpecificProductPage(){
+    let {id} = useParams();
+    
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default SpecificProductPage;
+
