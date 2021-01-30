8658dfdd36800b09f8672a33c6ca830db31a85fc
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2021-01-30 19:35:38 +0530

    ADD - Product pages added

--- src/pages/SpecficProductPage.js
+++ src/pages/SpecficProductPage.js
@@ -4,8 +4,15 @@ import {
   Center,
   Heading,
   HStack,
+  IconButton,
   Image,
+  Progress,
   SimpleGrid,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
   Text,
   useColorMode,
 } from "@chakra-ui/react";
@@ -13,29 +20,57 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import ImageLoader from "react-image-file";
-import { StarIcon } from "@chakra-ui/icons";
+import { AddIcon, MinusIcon, SearchIcon, StarIcon } from "@chakra-ui/icons";
+import CategorizedReviewPreview from "../components/CategorizedReviewPreview";
+import ReviewCountPreview from "../components/ReviewCountPreview";
 
 function SpecificProductPage() {
   let { id } = useParams();
   const { colorMode, toggleColorMode } = useColorMode();
+  
+  const [currentOrder, setCurrentOrder] = useState({ varient: 0, quantity: 0 });
+
+  const quantity_inc = () => {
+    setCurrentOrder({ ...currentOrder, quantity: currentOrder.quantity + 1 });
+  };
+  const quantity_dec = () => {
+    setCurrentOrder({ ...currentOrder, quantity: currentOrder.quantity - 1 });
+  };
 
   const [data, setData] = useState({
     item_ID: "233d",
     item_name: "Electric tooth brush",
     category: "electronic",
     price: 200.0,
+    orders: 345,
     description:
       "dqdasd a sadasd asdasdafsfrgs gsgsg sd gsgsgsdg sdgsdgsdg sdg",
     status: "Available",
     feedbacks: [
       {
         customer_name: "jof hagi",
+        customer_dp: "jof hagi",
         comment: "asdasds asdasd sad",
         rating: 5,
         reply: ["sfsdfsd", "adasdasd asdasdasfasc sadasdas"],
       },
       {
         customer_name: "den kali",
+        customer_dp: "jof hagi",
+        comment: "axasxas asdasd sad",
+        rating: 5,
+        reply: ["sfsdsddfsd", "asdassadasd sadssadasd sadasad"],
+      },
+      {
+        customer_name: "gendi gahnadi",
+        customer_dp: "jof hagi",
+        comment: "hgfhf dghhdgf dfgshssdd",
+        rating: 2,
+        reply: ["sfsdsddfsd", "asdassadasd sadssadasd sadasad"],
+      },
+      {
+        customer_name: "den kali",
+        customer_dp: "jof hagi",
         comment: "axasxas asdasd sad",
         rating: 2,
         reply: ["sfsdsddfsd", "asdassadasd sadssadasd sadasad"],
@@ -44,21 +79,24 @@ function SpecificProductPage() {
     variants: [
       {
         varient_name: "white 2 brushes",
-        image: "./images/White.jpg",
+        image:
+          "https://b3h2.scene7.com/is/image/BedBathandBeyond/69055867778_imageset?$690$&wid=690&hei=690",
         color: "green",
         details: "",
         Quantity: 450,
       },
       {
         varient_name: "pink 2 brushes with bateries",
-        image: "./images/pink.jpg",
+        image:
+          "https://www.powerplanetonline.com/cdnassets/xiaomi_soocas_x3u_sonic_electric_toothbrush_01_rosa_l.jpg",
         color: "pink",
         details: "",
         Quantity: 230,
       },
       {
         varient_name: "two heads only",
-        image: "./images/blue.jpg",
+        image:
+          "https://dam.which.co.uk/3b117c36-ca30-49bb-853c-9d31b220a90d.jpg",
         color: "blue",
         details: "",
         Quantity: 500,
@@ -66,26 +104,56 @@ function SpecificProductPage() {
     ],
   });
 
+  var imageStack = new Array(data.variants.length)
+    .fill(0)
+    .map((zero, index) => (
+      <Box
+        cursor="pointer"
+        borderWidth={currentOrder.varient === index ? "3px" : "1px"}
+        borderColor="gray.500"
+        onClick={() => setCurrentOrder({ ...currentOrder, varient: index })}
+      >
+        <Image
+          src={data.variants[index].image}
+          w="100px"
+          h="100px"
+          alt="image"
+        />
+      </Box>
+    ));
+
   return (
-    <Box pt="200px" pl="100px" pr="100px">
-      <SimpleGrid columns={2} spacing={5} minChildWidth="500px">
+    <Box
+      pt="200px"
+      pl={{ base: "10px", sm: "100px" }}
+      pr={{ base: "10px", sm: "100px" }}
+    >
+      <SimpleGrid columns={2} spacing={5} minChildWidth="400px">
         <Box
           width="auto"
-          h="80vh"
-          borderWidth="2px"
-          borderRadius="lg"
-          borderColor="black"
-          overflow="hidden"
-          p="5px"
-        ></Box>
-        <Box
-          width="auto"
-          h="80vh"
+          h="auto"
           borderWidth="2px"
           borderRadius="lg"
           borderColor="gray.300"
           overflow="hidden"
-          p="20px"
+          p="5px"
+        >
+          <Image
+            src={data.variants[currentOrder.varient].image}
+            w="auto"
+            h="auto"
+            alt="image"
+            fit
+          />
+        </Box>
+        <Box
+          width="auto"
+          h="auto"
+          borderWidth="2px"
+          borderRadius="lg"
+          borderColor="gray.300"
+          overflow="hidden"
+          p={{ base: "5px", sm: "20px" }}
         >
           <Heading as="h2" size="2xl">
             {data.item_name}
@@ -94,7 +162,6 @@ function SpecificProductPage() {
           <Text fontSize="xl" ml="3px" mt="5px">
             {data.category}
           </Text>
-
           <Box d="flex" mt="2" ml="2px" alignItems="center">
             {Array(5)
               .fill("")
@@ -104,10 +171,33 @@ function SpecificProductPage() {
                   color={i < data.feedbacks[0].rating ? "cyan.500" : "cyan.300"}
                 />
               ))}
-            <Box as="span" ml="2" color="gray.600" fontSize="sm">
-              {data.feedbacks.length} reviews
+            <Box as="span" ml="2"  fontSize="sm">
+              {data.feedbacks.length} reviews &bull; {data.orders} orders
             </Box>
           </Box>
+          <Heading as="h4" size="md" mt="20px">
+            Varient
+          </Heading>
+          <HStack mt="20px">{imageStack}</HStack>
+          <Heading as="h4" size="md" mt="20px">
+            Quantity
+          </Heading>
+          <HStack mt="10px">
+            <IconButton
+              size="xs"
+              aria-label="Search database"
+              icon={<AddIcon />}
+              onClick={quantity_inc}
+            />
+            <Text>{currentOrder.quantity}</Text>
+            <IconButton
+              size="xs"
+              aria-label="Search database"
+              icon={<MinusIcon />}
+              onClick={quantity_dec}
+            />
+          </HStack>
+
           <HStack mt="20px">
             <Button colorScheme="cyan" color="white" size="lg">
               Buy now
@@ -116,9 +206,6 @@ function SpecificProductPage() {
               Add to cart
             </Button>
           </HStack>
-          <Center>
-            <Image src={data.variants[0].image} alt="image" />
-          </Center>
 
           <Heading as="h4" size="md" mt="20px">
             Description
@@ -130,14 +217,53 @@ function SpecificProductPage() {
       </SimpleGrid>
       <Box
         width="auto"
-        h="50vh"
+        h="auto"
         borderWidth="2px"
         borderRadius="lg"
         borderColor="black"
         overflow="hidden"
-        p="5px"
+        p={{ base: "5px", sm: "20px" }}
         mt="20px"
-      ></Box>
+        mb="100px"
+
+      >
+        <Heading as="h2" size="2xl" mb='20px'>
+          Reviews
+        </Heading>
+        
+        <ReviewCountPreview feedbacks={data.feedbacks}/>
+          <Tabs>
+          <TabList>
+          <Tab><StarIcon color="cyan.500"/> All</Tab>
+            <Tab><StarIcon color="cyan.500"/> 5</Tab>
+            <Tab><StarIcon color="cyan.500"/> 4</Tab>
+            <Tab><StarIcon color="cyan.500"/> 3</Tab>
+            <Tab><StarIcon color="cyan.500"/> 2</Tab>
+            <Tab><StarIcon color="cyan.500"/> 1</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ='all' />
+            </TabPanel>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ={5} />
+            </TabPanel>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ={4} />
+            </TabPanel>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ={3} />
+            </TabPanel>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ={2} />
+            </TabPanel>
+            <TabPanel>
+              <CategorizedReviewPreview feedbacks={data.feedbacks} rating ={1} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Box>
   );
 }
