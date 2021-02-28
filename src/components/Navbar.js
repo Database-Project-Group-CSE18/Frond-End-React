dbea56ace4a9fa0115f289fbf18843fb07c610a9
Chalindu Kodikara <Chalindu.18@cse.mrt.ac.lk>
2021-03-01 03:31:06 +0530

    login goda

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import {
   Flex,
   Box,
-  Heading,
   Button,
   useColorMode,
   IconButton,
@@ -37,8 +36,8 @@ function Navbar(props) {
   const { colorMode, toggleColorMode } = useColorMode();
   console.log("Navigation bar")
   function handleLogOut() {
-    Axios.post("http://localhost:5000/customer/logout").then((response) => {
-      return <Redirect to='/signin' />
+    Axios.get("http://localhost:5000/customer/logout").then((response) => {
+      return <Redirect to='/' />
     })
   }
 
@@ -87,12 +86,13 @@ function Navbar(props) {
           </Link> : <Link as={ReactRouterLink} to="/customerdashboard">
             Dashboard
           </Link>}
-          
         </MenuItems>
         <MenuItems>
-          <Link as={ReactRouterLink} to="/">
-            About us
-          </Link>
+        {props.Auth.userID === 1 ? <Link as={ReactRouterLink} to="/sellerHome">
+            Seller Home
+          </Link> : <Link as={ReactRouterLink} to="/">
+            About Us
+          </Link>}
         </MenuItems>
         <MenuItems>
           <Link as={ReactRouterLink} to="/">
